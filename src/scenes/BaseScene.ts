0e7a648d2125572fd4bd3acb8a64e4911662eb11
Dj-Polyester <anonymous_571@outlook.com>
2023-01-30 17:03:22 +0300

    `addPhysics` implementation for `BaseScene`

--- src/scenes/BaseScene.ts
+++ src/scenes/BaseScene.ts
@@ -1,25 +1,20 @@
-import { CannonJSPlugin, Engine, Scene, Vector3 } from "babylonjs";
+import { CannonJSPlugin, Engine, IPhysicsEnginePlugin, Scene, Vector3 } from "babylonjs";
 import Controller from "../controllers/Controller";
-import PlayerController from "../controllers/PlayerController";
-import SpectatorViewController from "../controllers/SpectatorViewController";
+import TestController from "../controllers/TestController";
 import Player from "../objects/Player";
 import * as CANNON from "cannon";
-import { FPS_COUNT_ } from "../globals";
-
+import EntityObject from "../objects/EntityObject";
+import { FPS_COUNT_, GRAVITY, TIME_STEP_ } from "../globals";
 export default class BaseScene extends Scene {
-  player: Player;
-  controller: Controller;
+  physEngine: IPhysicsEnginePlugin;
   constructor(engine: Engine, canvas: any) {
     super(engine);
-    const physEngine = new CannonJSPlugin(true, 10, CANNON);
-    this.enablePhysics(new Vector3(0, -9.81, 0), physEngine);
-
-    physEngine.setTimeStep(1 / FPS_COUNT_);
-
-    // This creates and positions a free camera (non-mesh)
-    this.player = new Player(this);
-    this.controller = new SpectatorViewController(this, canvas);
-
-    this.player.addController(this.controller);
+    this.physEngine = new CannonJSPlugin(true, 10, CANNON);
+  }
+  addPhysics() {
+    console.log(`GRAVITY: ${GRAVITY}`);
+    this.gravity = GRAVITY;
+    this.enablePhysics(GRAVITY, this.physEngine);
+    this.physEngine.setTimeStep(TIME_STEP_);
   }
 }
