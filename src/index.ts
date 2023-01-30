745eab24920434dc07b66a205fcde1bd1b7a64fd
Dj-Polyester <anonymous_571@outlook.com>
2023-01-30 17:02:28 +0300

    No idea what this is (can be discussed later)

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import TestScene from "./scenes/TestScene";
 export const start = async () => {
   if (Engine.isSupported()) {
     const canvas: any = document.getElementById("canvas");
-    console.log(`index.ts:8:canvas: ${canvas}`);
-    const engine = new Engine(canvas, true);
+    const engine = new Engine(canvas, true, {
+      deterministicLockstep: true,
+      lockstepMaxSteps: 4
+    });
 
     const scene = new TestScene(engine, canvas);
     // Hide loading icon
